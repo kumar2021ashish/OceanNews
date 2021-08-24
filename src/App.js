7bf7c39d9705df6da3057650ff1b5cb972cbaaa6
ashish2020kashyap <ashish2020kashyap@gmail.com>
2021-08-24 16:24:21 +0530

    ui fix

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Nav from './nav';
 import Question from './Question';
-
-
+import Grid from '@material-ui/core/Grid';
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/Card";
 const Quiz = () =>{
   const [questions,setQuestions]=useState(null);
   const [count]=useState(0);
@@ -22,25 +23,46 @@ const Quiz = () =>{
     console.log(questions);
   },[questions,count]);
   if(!questions) return <p className="text-light text-muted">Loading...</p>;
-  //setCount(count+1);
+
   return (
     <div>
       < Nav />
-    <h1 className="text-center">OceanNews</h1> 
-<Question question={questions[count]} />
-<div className="card-text">
-  <div
-  className="btn-group btn-group-vertical btn-group-toggle w-100"
-  data-toggle="buttons"
-  >
 
-  </div>
+    
+    <h1 className="text-center">OceanNews</h1> 
+    {questions.map((item) =>
+    <div class="container">
+       <div class="row">
+    <div class="col-sm">
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={item.imageurl} Style="width:100%" />
+    <Card.Body>
+    <Card.Title>{item.title}</Card.Title>
+    <Card.Text>
+    {item.description}
+    </Card.Text>
+    <p class="card-text"><small class="text-muted">Author : {item.author}</small></p>
+    <p class="card-text"><small class="text-muted">Publish Date : {item.publishdate}</small></p>
+  </Card.Body>
+</Card>
+</div>
+</div>
+<br></br><br></br><br></br><br></br><br></br>
 </div>
 
+
+    )}
+
+
+
+
     </div>
+
     
   );
 } 
 
 export default Quiz;
+
+
 
--- src/Question.js
+++ src/Question.js
@@ -1,22 +1,31 @@
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
+//import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Card from "react-bootstrap/Card";
 
-const Question=({question,title}) =>{
-    return (
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={question.imageurl} />
-  <Card.Body>
-    <Card.Title>{question.title}</Card.Title>
-    <Card.Text>
-    {question.description}
-    </Card.Text>
-    <p class="card-text"><small class="text-muted">Author : {question.author}</small></p>
-    <p class="card-text"><small class="text-muted">Publish Date : {question.publishdate}</small></p>
-  </Card.Body>
-</Card>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-    )
+const Question=({question}) =>{
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+j
+ 
+    </div>
+  );
+
+
 };
 
 export default Question;
